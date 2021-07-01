7ce88db737d6eff3f6fac249e33731594849d565
abi0116 <77713619+abi0116@users.noreply.github.com>
2021-07-01 14:47:01 +0900

    5 データ型

---
+++ MyApp5.java
@@ -0,0 +1,21 @@
+public class MyApp5 {
+
+  public static void main(String[] args) {
+    // 文字
+    char a = 'a';
+    // 整数 byte short int long　右に行くほど使える桁が大きい(基本int)
+    int x = 10;
+    long y = 5555555555L;
+    // 浮動小数点数 float double　右に行くほど使える桁が大きい(基本double)
+    double d = 23423.344;
+    float f = 32.33F;
+    // 論理値
+    boolean flag = true; // false
+    // 文字列
+    // \n 改行
+    // \t tab文のスペース
+    String msg = "Hello Wo\nrld Again\t!";
+    System.out.println(msg);
+  }
+
+}
