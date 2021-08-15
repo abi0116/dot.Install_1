d2a04ecd638324b6aa4cb9d88c7ec6d181546186
abi0116 <77713619+abi0116@users.noreply.github.com>
2021-08-15 10:56:28 +0900

    Myapp28.java[add]

--- MyApp28.java
+++ MyApp28.java
@@ -1,19 +1,22 @@
-// 抽象クラス -> 具象クラス
+// 抽象クラス -> 具象クラス(抽象クラスを継承するクラスのこと)
+//抽象クラスは他のクラスに継承してもらうことが前提のクラス(自信をインスタンス化出来ない)
 
-abstract class User {
-  public abstract void sayHi(); // 抽象メソッド
-}
+//今回はこのUserクラスを抽象クラスにして、JapanユーザークラスとAmericanユーザークラスを作る
+abstract class User { //今回はフィールドは無し
+  public abstract void sayHi(); // 抽象メソッド(具象クラスに実装を任せるメソッドのこと)
+ // こっちにもabstractを付けると、具象クラスで実装されなかった時にエラーにしてくれる
+}//働きとしては、関連する部分の切り出しを担っている
 
 class JapaneseUser extends User {
   @Override
-  public void sayHi() {
+  public void sayHi() { //抽象メソッドをオーバーライド(しないとコンパイルエラーになる)
     System.out.println("こんにちは！");
   }
 }
 
 class AmericanUser extends User {
   @Override
-  public void sayHi() {
+  public void sayHi() { //抽象メソッドをオーバーライド(しないとコンパイルエラーになる)
     System.out.println("Hi!");
   }
 }
@@ -28,3 +31,7 @@ public class MyApp {
   }
 
 }
+
+//実行結果　$ javac Myapp.java  $ java Myapp
+//Hi!
+//こんにちは！
